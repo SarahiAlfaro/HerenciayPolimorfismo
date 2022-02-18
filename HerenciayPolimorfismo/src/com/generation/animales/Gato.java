314e30ce6d28d79cab3e1897ab2e6c081b7669e0
Sarahi Alfaro <alfarod.sarahi@gmail.com>
2022-02-17 19:37:01 -0600

    Ultima actualización con todos los ejercicios

---
+++ HerenciayPolimorfismo/src/com/generation/animales/Gato.java
@@ -0,0 +1,22 @@
+package com.generation.animales;
+
+public class Gato extends Animal implements Mascota{
+	@Override
+	public void jugar() {
+		System.out.println("Estoy jugando con cajas");
+		
+	}
+
+	@Override
+	public void correr() {
+		System.out.println("Estoy persiguiendo una luz");
+		
+	}
+
+	@Override
+	public void baniar() {
+		System.out.println("No me gusta el agua :-(");
+		
+	}
+
+}
